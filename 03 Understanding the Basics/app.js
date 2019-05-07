30579ba70eac9ac358551b3213aa520fe1781821
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-07 22:23:34 +0530

    9C - Response

--- 03 Understanding the Basics/app.js
+++ 03 Understanding the Basics/app.js
@@ -1,7 +1,23 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
+    const url = req.url;
+    console.log('Url Received', url);
+    if(url === '/'){
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<header><title>Enter Message</title></header>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<header><title>My First Page</title>');
+    res.write('</header>');
+    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000)
