e173c235208f13289afcde486193c52609211777
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-07 22:31:27 +0530

    10C - Redirect

--- 03 Understanding the Basics/app.js
+++ 03 Understanding the Basics/app.js
@@ -1,7 +1,9 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     console.log('Url Received', url);
     if(url === '/'){
         res.setHeader('Content-type', 'text/html');
@@ -9,6 +11,12 @@ const server = http.createServer((req, res) => {
         res.write('<header><title>Enter Message</title></header>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-type', 'text/html');
