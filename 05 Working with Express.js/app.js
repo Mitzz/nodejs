1696854cde9c91a7251c1b34e8d29dda0782a412
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-19 20:21:41 +0530

    28C - 05_Request Body Parser

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
