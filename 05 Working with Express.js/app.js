bb768a8f3546c7e59c139311cbf1e32e33ec667a
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-19 19:48:03 +0530

    24C - 05_Middleware and Chaining

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -1,10 +1,22 @@
-const http = require('http')
-const fs = require('fs')
-const routes = require('./routes')
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("in the middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("in another middleware");
+});
+
+const server = http.createServer(app);
+
+
 
 const PORT_NO = 3000;
-const server = http.createServer(routes.handler);
-
 server.listen(PORT_NO,  function(){
     console.log("Server listening on port " + PORT_NO);
 });
