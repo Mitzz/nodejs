be6792afe6c44ba9cbd7b2c4d3b31055fc7ecd43
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-19 11:49:18 +0530

    16C - Set up code for chapter '04 Improved Development Workflow and Debugging'
    Set-up code

--- 04 Improved Development Workflow and Debugging/routes.js
+++ 04 Improved Development Workflow and Debugging/routes.js
@@ -7,7 +7,7 @@ const requestHandler = (req, res) => {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form><form action="/deleteimage" method="POST"><input type="text" name="deleteimage"><input type="text" name="basedir"><button type="submit">Delete Image</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
     return res.end();
@@ -28,22 +28,6 @@ const requestHandler = (req, res) => {
       });
     });
   }
-  if (url === '/deleteimage' && method === 'POST') {
-    const body = [];
-    req.on('data', chunk => {
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString('base64');
-      console.log(parsedBody);
-	  const message = parsedBody.split('&')[1];
-      fs.appendFile('deletedImage.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
@@ -51,16 +35,6 @@ const requestHandler = (req, res) => {
   res.write('</html>');
   res.end();
 };
-
-//module.exports = requestHandler;
-
-// module.exports = {
-//     handler: requestHandler,
-//     someText: 'Some hard coded text'
-// };
-
-// module.exports.handler = requestHandler;
-// module.exports.someText = 'Some text';
 
 exports.handler = requestHandler;
 exports.someText = 'Some hard coded text';
