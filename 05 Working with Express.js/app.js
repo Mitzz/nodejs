c7743209128019dbc5f3d55a81e6f37de94ac40f
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-19 20:16:26 +0530

    27C - 05_Request Body undefined

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -8,10 +8,14 @@ app.use('/', (req, res, next) => {
 });
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In add-product middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  });
+  
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
-
+  
 app.use('/', (req, res, next) => {
   console.log('In root-url middleware!');
   res.send('<h1>Hello from Express!</h1>');
