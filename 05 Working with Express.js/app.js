792ee1b3d4b13e296ec30f05d32a107dcf37271b
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-19 20:12:05 +0530

    26C - 05_Handling multiple routes and their order

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("in the middleware");
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log("in another middleware");
-    res.send("<h1>Hello from Express.js</h1>");
+app.use('/add-product', (req, res, next) => {
+  console.log('In add-product middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In root-url middleware!');
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 const PORT_NO = 3000;
