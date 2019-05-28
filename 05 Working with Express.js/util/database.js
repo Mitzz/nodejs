f44d7e7ae652abb9538f008523bf9e1593428d37
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-28 09:10:28 +0530

    70C - Fetch all products from DB

--- 05 Working with Express.js/util/database.js
+++ 05 Working with Express.js/util/database.js
@@ -3,9 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node',
-    password: 'Diebold@123',
-    port: 3310
+    database: 'node-complete',
+    password: 'nodecomplete'
 });
 
 module.exports = pool.promise();
