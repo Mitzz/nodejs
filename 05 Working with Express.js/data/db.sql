456204747b9ade4e8254d31769396e92d0dc9693
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-28 09:02:55 +0530

    69C - Database - Setup, Module, Connect and Execute

---
+++ 05 Working with Express.js/data/db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `node`.`products` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `price` DOUBLE NOT NULL,
+  `description` TEXT NOT NULL,
+  `imageUrl` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+INSERT INTO `node`.`products` (`title`, `price`, `description`, `imageUrl`) VALUES ('A Book', '12.99', 'This is an amazing book', 'lsdf');
---
+++ 05 Working with Express.js/util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node',
+    password: 'Diebold@123',
+    port: 3310
+});
+
+module.exports = pool.promise();
