699a221057b5822347d70eba466be7bd2a2aa612
Bhansali, Mithul <Mithul.Bhansali@dieboldnixdorf.com>
2019-05-28 10:05:47 +0530

    73C - Sequelize setup and model definition

--- 05 Working with Express.js/util/database.js
+++ 05 Working with Express.js/util/database.js
@@ -1,11 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node',
-    password: 'Diebold@123',
-    port:3310
+const sequelize = new Sequelize('node', 'root', 'Diebold@123', {
+  dialect: 'mysql',
+  host: 'localhost',
+  port: 3310
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
